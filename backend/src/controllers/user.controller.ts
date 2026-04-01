8d0bfee9e9f17d900e3f84a43451b6ece5aae937
Aman Madhukar <amanmadhukar63@gmail.com>
2026-04-01 23:01:44 +0530

    validaton added in signup controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
+import bcrypt from 'bcrypt';
 import User from "../models/user.model.js";
+import { signupSchema } from "../helper/validation.js";
+import z from "zod";
 
 export async function signup(
   req: Request,
@@ -8,45 +11,49 @@ export async function signup(
 
   try {
 
-    const { name, email, password } = req.body;
+    const parsedData = signupSchema.safeParse(req.body);
 
-    if(!name || !email || !password) {
+    if(!parsedData.success) {
       res.status(400).json({
-        msg: "Required fields are missing",
-        data: {
-          name,
-          email,
-          password
-        }
+        msg: "Invalid Input",
+        error: z.treeifyError(parsedData.error)
       });
       return;
     }
 
-    let user;
+    const { name, email, password } = parsedData.data;
 
-    user = await User.find({
-      email
-    });
+    const existingUser = await User.findOne({ email });
 
-    if(user) {
-      res.status(400).json({
+    if(existingUser) {
+      res.status(409).json({
         msg: "User already exist, Try to login"
       });
       return;
     }
 
-    user = await User.create({
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
       name,
       email,
-      password
+      password: hashedPassword
     });
 
-    res.status(200).json({
+    const userResponse = {
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+    };
+
+    res.status(201).json({
       msg: "User created successfully",
-      data: user
+      data: userResponse
     });
     
   } catch (error) {
+    console.error("SignUp Error: ", error);
+
     res.status(500).json({
       msg: "Server Error",
       err: error
