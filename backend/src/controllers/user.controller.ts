46442cc3d833c3670cbaaabbddfc3d03d41771f8
Aman Madhukar <amanmadhukar63@gmail.com>
2026-04-05 09:17:59 +0530

    logout controller added

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -125,6 +125,17 @@ export async function login(
 
 }
 
-export function logout(){
-  
+export function logout(
+  req: Request,
+  res: Response
+) : void {
+  try {
+    responseHandler(res, {
+      message: "Logout Successfully",
+      statusCode: 201,
+      data: {}
+    });
+  } catch (error) {
+    console.error("Error while logout", error);
+  }
 }
