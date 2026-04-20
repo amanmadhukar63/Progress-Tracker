349ed6472da30c21489a395325411e362c9e8793
Aman Madhukar <amanmadhukar63@gmail.com>
2026-04-20 21:47:28 +0530

    side bar ui completed

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,17 +1,20 @@
 import type { ReactElement } from "react";
 import "./Button.scss";
+import type React from "react";
 
 interface IButton {
   type: "button" | "submit";
   title: String;
+  style?: React.CSSProperties;
 }
 
 export default function Button({
   title,
   type = "button",
+  style = {}
 }: IButton) : ReactElement {
   return (
-    <button type={type} className="button">
+    <button type={type} className="button" style={style}>
       {title}
     </button>
   );
