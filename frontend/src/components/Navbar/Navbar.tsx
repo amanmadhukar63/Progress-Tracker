e7f134904a928bdf745dbc8556ddb7675485994c
Aman Madhukar <amanmadhukar63@gmail.com>
2026-04-22 12:14:55 +0530

    navbar completed

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,13 +1,18 @@
 import "./Navbar.scss";
+import NotificationIcon from "../../assets/notification-icon.svg";
+import NotificationUnreadIcon from "../../assets/notification-unread-icon.svg";
+import SearchIcon from "../../assets/search-icon.svg";
 
 export default function Navbar() {
   return (
     <div className="navbar-container">
       <div className="navbar-container__search-bar">
-        Search
+        <img src={SearchIcon} alt="search-icon" />
+        <div className="placeholder-text">Search tasks, goals, or metrics...</div>
       </div>
       <div className="navbar-container__notification">
-        Notify
+        <img src={NotificationIcon} alt="notification" />
+        <img className="notification-unread" src={NotificationUnreadIcon} alt="notification-unread" />
       </div>
     </div>
   );
