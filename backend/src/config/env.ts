a589251908a12e9cd203e67082566cbcf9254081
Aman Madhukar <amanmadhukar63@gmail.com>
2026-04-30 21:16:48 +0530

    fixed cors error

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -4,4 +4,6 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 
+export const MONGO_URI = process.env.MONGO_URI || "";
+
 export const JWT_SECRET = process.env.JWT_SECRET || 'super_secret_key';
--- backend/src/helper/db.ts
+++ backend/src/helper/db.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import { MONGO_URI } from "../config/env.js";
 
 export async function connectDB(){
   try {
 
-    await mongoose.connect(process.env.MONGO_URI || "");
+    await mongoose.connect(MONGO_URI);
     console.log("✅ MongoDB connected");
 
   } catch (error) {
