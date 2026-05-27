968d85d0b302a78cde642725a790cc1335974d3b
Aman Madhukar <amanmadhukar63@gmail.com>
2026-05-27 19:30:21 +0530

    added tan stack query setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
@@ -9,8 +13,12 @@ import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/800.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient} >
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
